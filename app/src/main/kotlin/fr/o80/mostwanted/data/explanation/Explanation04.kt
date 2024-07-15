491e0f229744d65783e9c114d2ad4f88ce636944
AudreyGentili <audrey.gentili@zenika.com>
2024-07-15 09:47:06 +0200

    feat: add fourth exercise about Preview

--- app/src/main/kotlin/fr/o80/mostwanted/data/explanation/Explanation04.kt
+++ app/src/main/kotlin/fr/o80/mostwanted/data/explanation/Explanation04.kt
@@ -4,12 +4,63 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import fr.o80.mostwanted.data.Code
+import fr.o80.mostwanted.data.ExplanationText
 
 @Composable
 fun Explanation04() {
     Column(
         Modifier.fillMaxSize()
     ) {
-
+        ExplanationText(
+            """Vous savez maintenant créer un écran avec plusieurs 
+                Composables à l'intérieur !
+                """
+        )
+        ExplanationText(
+            """Comme vous l'avez peut-être remarqué, voir les modifications 
+                de votre code immédiatement dans votre application n'est pas 
+                évident...
+                 Vous devez relancer l'application et cela peut prendre du 
+                 temps...
+                """
+        )
+        ExplanationText(
+            """Mais Compose est là pour améliorer votre expérience de 
+                développeur ! En effet, il apporte un système de 
+                <b>Preview</b> directement accessible depuis votre IDE afin 
+                de prévisualiser vos Composables de manière dynamique.
+                """
+        )
+        ExplanationText(
+            """Pour cela, il vous suffit de créer une nouvelle méthode 
+                annotée d'un <b>@Preview</b>, puis d'appeler le Composable 
+                que vous souhaitez visualiser à l'intérieur :
+                """
+        )
+        Code("@Preview\n" +
+                   "@Composable\n" +
+                   "private fun Preview() {\n" +
+                   "   Composable()\n" +
+                   "}")
+        ExplanationText(
+            """Récupérez un Composable développé lors d'un exercice précédent
+                 puis amusez-vous à l'afficher au sein de quelques Preview, en 
+                 jouant sur les paramètres proposés par l'annotation 
+                 <b>@Preview</b>.
+                """
+        )
+        ExplanationText(
+            """<i>À noter : Il est préférable de rendre votre fonction privée, 
+                car 
+                elle sera accessible seulement au sein de ce fichier.</i>
+                """
+        )
+        ExplanationText(
+            """<i>Jetpack Compose intègre aussi désormais un système de 
+                <b>Hot-Reload</b> sur votre application, à la manière de 
+                Flutter.</i>
+                """
+        )
     }
 }
