28d618100ba52337a5a232316f9cbf5788d081a6
Olivier PEREZ <olivier@olivierperez.fr>
2024-07-17 09:37:41 +0200

    feat: Add back button on exercises (#8)

--- app/src/main/kotlin/fr/o80/mostwanted/detail/component/page/ExerciseDetailLoaded.kt
+++ app/src/main/kotlin/fr/o80/mostwanted/detail/component/page/ExerciseDetailLoaded.kt
@@ -5,16 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
-import fr.o80.mostwanted.component.ScreenTitle
 import fr.o80.mostwanted.data.ExplanationText
 import fr.o80.mostwanted.detail.component.template.ExerciseResult
 import fr.o80.mostwanted.detail.component.template.ExerciseSketchup
@@ -22,11 +27,12 @@ import fr.o80.mostwanted.domain.model.ExerciseDef
 import fr.o80.mostwanted.ui.theme.WorkshopMostWantedPreviewTheme
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ExerciseDetailLoaded(
     exerciseDef: ExerciseDef,
     page: DetailPage,
+    goBack: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     val scope = rememberCoroutineScope()
@@ -39,8 +45,18 @@ fun ExerciseDetailLoaded(
     Column(
         modifier = modifier
     ) {
-        ScreenTitle(
-            title = exerciseDef.title
+        TopAppBar(
+            title = {
+                Text(
+                    text = exerciseDef.title,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = goBack) {
+                    Icon(Icons.AutoMirrored.Default.KeyboardArrowLeft, contentDescription = "Backs")
+                }
+            }
         )
         TabRow(selectedTabIndex = pagerState.currentPage) {
             Tab(
@@ -94,6 +110,7 @@ fun ExerciseDetailLoadedPreview() {
                 solution = {}
             ),
             page = DetailPage.Result,
+            goBack = {},
             modifier = Modifier.fillMaxSize()
         )
     }
