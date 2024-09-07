be679f270a5f923716d29a6a2f2b3c6104e5789a
Olivier PEREZ <olivier@olivierperez.fr>
2024-09-07 10:22:05 +0200

    feat: fix displaying of result

--- app/src/main/kotlin/fr/o80/mostwanted/detail/component/template/ExerciseResult.kt
+++ app/src/main/kotlin/fr/o80/mostwanted/detail/component/template/ExerciseResult.kt
@@ -1,21 +1,12 @@
 package fr.o80.mostwanted.detail.component.template
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import fr.o80.mostwanted.data.ExplanationText
@@ -31,7 +22,7 @@ fun ExerciseResult(
     ResultFrame(
         modifier = modifier.padding(16.dp)
     ) {
-        exerciseDef.result
+        exerciseDef.result()
     }
 }
 
