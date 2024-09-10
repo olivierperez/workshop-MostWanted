8c9bc63c9c05808d5b96cb48d5915087506113eb
Olivier PEREZ <olivier@olivierperez.fr>
2024-09-10 21:18:38 +0200

    feat: simplify image solutions

--- exercises/src/main/kotlin/fr/o80/mostwanted/solutions/Solution04-02Image.kt
+++ exercises/src/main/kotlin/fr/o80/mostwanted/solutions/Solution04-02Image.kt
@@ -1,16 +1,12 @@
 package fr.o80.mostwanted.solutions
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import fr.o80.mostwanted.internals.PreviewTheme
 import fr.o80.mostwanted.internals.domain.model.Avatar
 
@@ -19,17 +15,11 @@ fun Solution0402Image(
     avatar: Avatar,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Image(
-            painter = painterResource(avatar.drawable),
-            contentDescription = "Avatar",
-            modifier = modifier.clip(CircleShape)
-        )
-    }
+    Image(
+        modifier = modifier.clip(CircleShape),
+        painter = painterResource(avatar.drawable),
+        contentDescription = "Avatar"
+    )
 }
 
 @Preview
--- exercises/src/main/kotlin/fr/o80/mostwanted/solutions/Solution04-03Image.kt
+++ exercises/src/main/kotlin/fr/o80/mostwanted/solutions/Solution04-03Image.kt
@@ -2,20 +2,16 @@ package fr.o80.mostwanted.solutions
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import fr.o80.mostwanted.internals.PreviewTheme
 import fr.o80.mostwanted.internals.domain.model.Avatar
 
@@ -24,21 +20,15 @@ fun Solution0403Image(
     avatar: Avatar,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        var corner by remember { mutableIntStateOf(0) }
+    var corner by remember { mutableIntStateOf(0) }
 
-        Image(
-            painter = painterResource(avatar.drawable),
-            contentDescription = "Most Wanted Avatar",
-            modifier = Modifier
-                .clip(RoundedCornerShape(corner))
-                .clickable { corner = 50 - corner }
-        )
-    }
+    Image(
+        modifier = modifier
+            .clip(RoundedCornerShape(corner))
+            .clickable { corner = 50 - corner },
+        painter = painterResource(avatar.drawable),
+        contentDescription = "Avatar"
+    )
 }
 
 @Preview
