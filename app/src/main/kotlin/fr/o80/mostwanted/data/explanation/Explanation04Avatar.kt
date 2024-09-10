e7bd5e95ff4d80b4aba5fa20924016d06b55f892
Olivier PEREZ <olivier@olivierperez.fr>
2024-09-10 22:32:51 +0200

    feat: rewrite the avatar lore (2/3)

--- app/src/main/kotlin/fr/o80/mostwanted/data/explanation/Explanation04Avatar.kt
+++ app/src/main/kotlin/fr/o80/mostwanted/data/explanation/Explanation04Avatar.kt
@@ -62,26 +62,22 @@ fun ExplanationAvatarClipped() {
     ) {
         ExplanationTitle("Clip d'une image")
         ExplanationText(
-            """En se basant sur le code que vous avez développé lors de 
-                l'exercice précédent, nous allons essayer d'améliorer le 
+            """En se basant sur le code que vous avez produit lors de
+                l'exercice précédent, nous allons essayer d'améliorer le
                 rendu visuel de notre image.
                 """
         )
         ExplanationText(
-            """Nous aimerions par exemple la rendre ronde. Pour cela, rien de
-                 plus simple. Il vous suffit d'ajouter un <b>clip</b> sur 
-                 l'image pour modifier sa forme.
+            """Ajoutez un modifier <b>clip</b> au composable de l'avatar
+                    pour modifier sa forme. Un clip <b>CircleShape</b> permettra
+                    d'arrondir l'avatar.
                 """
         )
+        Code("""Modifier.clip(CircleShape)""")
         ExplanationText(
-            """Vous pouvez donc ajouter un <b>modifier</b> à votre image en 
-                lui attribuant un clip de type <b>CircleShape</b>
-                """
-        )
-        ExplanationText(
-            """Comme vous pouvez le constater, vous pouvez attribuer ce 
-                comportement à un modifier. Vous pouvez donc arrondir les bords
-                 de n'importe quel Composable !
+            """Note&nbsp;: <i>Vous pouvez vous en douter, vous pouvez appliquer ce
+                    comportement à n'importe quel Modifier ce qui implique que vous pouvez
+                    arrondir les bords de n'importe quel Composable&nbsp;!</i>
                 """
         )
     }
