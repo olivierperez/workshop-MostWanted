7d70fb7edbe26621813b06cd8858df40eb6a0aa8
Audrey Gentili <audrey.gentili@zenika.com>
2024-10-01 16:01:41 +0200

    feat: add previews in exercise files (#21)

--- exercises/src/main/kotlin/fr/o80/mostwanted/exercises/CIABadge.kt
+++ exercises/src/main/kotlin/fr/o80/mostwanted/exercises/CIABadge.kt
@@ -2,8 +2,18 @@ package fr.o80.mostwanted.exercises
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fr.o80.mostwanted.internals.PreviewTheme
 
 @Composable
 fun CIABadge(modifier: Modifier = Modifier) {
 
+}
+
+@Preview
+@Composable
+private fun CIABadgePreview() {
+    PreviewTheme {
+        CIABadge()
+    }
 }
--- exercises/src/main/kotlin/fr/o80/mostwanted/exercises/CIASkill.kt
+++ exercises/src/main/kotlin/fr/o80/mostwanted/exercises/CIASkill.kt
@@ -2,6 +2,8 @@ package fr.o80.mostwanted.exercises
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fr.o80.mostwanted.internals.PreviewTheme
 
 @Composable
 fun CIASkill(
@@ -12,4 +14,16 @@ fun CIASkill(
     modifier: Modifier = Modifier
 ) {
 
+}
+
+@Preview
+@Composable
+private fun CIASkillPreview() {
+    PreviewTheme {
+        CIASkill(
+            label = "",
+            level = "",
+            isBest = false
+        )
+    }
 }
