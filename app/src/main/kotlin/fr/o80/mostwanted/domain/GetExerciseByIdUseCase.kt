821ef6246e0e7c87aa9d4f7565e75e4e4617345b
Olivier PEREZ <olivier@olivierperez.fr>
2024-10-06 16:17:00 +0200

    feat: add an exercise for examiners

--- app/src/main/kotlin/fr/o80/mostwanted/domain/GetExerciseByIdUseCase.kt
+++ app/src/main/kotlin/fr/o80/mostwanted/domain/GetExerciseByIdUseCase.kt
@@ -1,6 +1,7 @@
 package fr.o80.mostwanted.domain
 
 import fr.o80.mostwanted.data.ExercisesRepository
+import fr.o80.mostwanted.data.intro
 import fr.o80.mostwanted.domain.model.ExerciseDef
 import javax.inject.Inject
 
@@ -8,6 +9,10 @@ class GetExerciseByIdUseCase @Inject constructor(
     private val exercisesRepository: ExercisesRepository
 ) {
     operator fun invoke(id: Int): ExerciseDef? {
+        if (id < 0) {
+            return intro
+        }
+
         return exercisesRepository.listAll()
             .flatMap { (_, exercises) -> exercises }
             .firstOrNull { it.id == id }
