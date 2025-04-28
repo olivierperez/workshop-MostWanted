71f16c11603e8cd413338be5946766ab88db0e97
Olivier PEREZ <olivier@olivierperez.fr>
2025-04-28 23:44:05 +0200

    feat: animate transition between pages

--- composeApp/src/commonMain/kotlin/fr/o80/mostwanted/solutions/solution07FinalMission/Solution07FinalMission.kt
+++ composeApp/src/commonMain/kotlin/fr/o80/mostwanted/solutions/solution07FinalMission/Solution07FinalMission.kt
@@ -1,16 +1,19 @@
 package fr.o80.mostwanted.solutions.solution07FinalMission
 
+import Page
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import fr.o80.mostwanted.internals.PreviewTheme
 import fr.o80.mostwanted.internals.domain.model.Avatar
@@ -24,8 +27,10 @@ fun Solution07FinalMission(
     modifier: Modifier = Modifier
 ) {
     var currentPage by rememberSaveable { mutableStateOf(Page.IDENTITY) }
+    val layoutType = navigationLayoutType()
     NavigationSuiteScaffold(
         modifier = modifier,
+        layoutType = layoutType,
         navigationSuiteItems = {
             Page.entries.forEach { page ->
                 item(
@@ -42,12 +47,37 @@ fun Solution07FinalMission(
             }
         },
     ) {
-        when (currentPage) {
-            Page.IDENTITY -> SolutionIdentity(firstName, lastName, avatar)
-            Page.MISSIONS -> SolutionMissionsList()
+        AnimatedContent(
+            targetState = currentPage,
+            transitionSpec = contentTransitionSpec(layoutType)
+        ) {
+            when (it) {
+                Page.IDENTITY -> SolutionIdentity(firstName, lastName, avatar)
+                Page.MISSIONS -> SolutionMissionsList()
+            }
         }
     }
 }
+
+@Composable
+private fun navigationLayoutType(): NavigationSuiteType =
+    NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo(currentWindowAdaptiveInfo())
+
+@Composable
+private fun contentTransitionSpec(layoutType: NavigationSuiteType): AnimatedContentTransitionScope<Page>.() -> ContentTransform =
+    remember(layoutType) {
+        {
+            val fadeIn = fadeIn(animationSpec = tween(220, delayMillis = 90))
+            val fadeOut = fadeOut(animationSpec = tween(90))
+            val slideInFromLeft = slideInHorizontally { -it / 10 }
+            val slideInFromBottom = slideInVertically { it / 10 }
+
+            val enterTransition = if (layoutType == NavigationSuiteType.NavigationRail) fadeIn + slideInFromLeft else fadeIn + slideInFromBottom
+            val exitTransition = fadeOut
+
+            enterTransition.togetherWith(exitTransition)
+        }
+    }
 
 @Preview
 @Composable
